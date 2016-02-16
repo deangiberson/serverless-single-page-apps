01d2fa3c766c4163d3241316c27c3e2e084bfdb3
Dean Giberson <dean@deangiberson.com>
2016-02-15 22:34:26 -0800

    Add routing

--- public/app.js
+++ public/app.js
@@ -2,7 +2,16 @@
 
 var learnjs = {};
 
+learnjs.problemjs = function() {
+    return $('<div class="problem-view">').text('Coming soon!');
+};
+
 learnjs.showView = function(hash) {
-    var problemView = $('<div class="problem-view">').text('Coming soon!');
-    $('.view-container').empty().append(problemView);
+    var routes = {
+	'#problem-1' : learnjs.problemjs
+    };
+    var viewFn = routes[hash];
+    if (viewFn) {
+	$('.view-container').empty().append(viewFn);
+    }
 };
--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -1,6 +1,11 @@
 describe('LearnJS', function() {
-    it('Can show a problem view', function() {
+    it('can show a problem view', function() {
 	learnjs.showView('#problem-1');
 	expect($('.view-container .problem-view').length).toEqual(1);
+    });
+
+    it('shows the landing page view when there is no hash', function() {
+	learnjs.showView('');
+	expect($('.view-container .landing-view').length).toEqual(1);
     });
 });
