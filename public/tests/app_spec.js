78ef8aed2a6ac777d5698edcf5375f1f58cae9d2
Dean Giberson <dean@deangiberson.com>
2016-02-18 23:10:52 -0800

    fix unit test

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -31,7 +31,7 @@ describe('LearnJS', function() {
     describe('problem view', function() {
 	it('has a title that includes problem number', function() {
 	    var view = learnjs.problemView('1');
-	    expect(view.text()).toEqual('Problem #1 Coming Soon!');
+	    expect(view.text().trim()).toEqual('Problem #1');
 	});
     });
 });
