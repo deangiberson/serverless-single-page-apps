be78868c3325bd32c93947eee958a2caae60ca60
Dean Giberson <dean@deangiberson.com>
2016-02-20 22:52:29 -0800

    Handling user input

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -45,5 +45,19 @@ describe('LearnJS', function() {
 	it('shows the problem code', function() {
 	    expect(view.find('[data-name="code"]').text()).toEqual('function problem() { return __; }');
 	});
+
+	describe('answer section', function() {
+	    it('can check a correct answer by hitting a button', function() {
+		view.find('.answer').val('true');
+		view.find('.check-btn').click();
+		expect(view.find('.result').text()).toEqual('Correct!');
+	    });
+
+	    it('rejects an incorrect answer', function() {
+		view.find('.answer').val('false');
+		view.find('.check-btn').click();
+		expect(view.find('.result').text()).toEqual('Incorrect!');
+	    });
+	});
     });
 });
