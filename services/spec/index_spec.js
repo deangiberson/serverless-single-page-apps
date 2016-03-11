5290fa8ce727771269c20f8915c21a21b390926b
Dean Giberson <dean@deangiberson.com>
2016-03-10 21:44:24 -0800

    Invoking Lambda Functions

--- services/spec/index_spec.js
+++ services/spec/index_spec.js
@@ -14,3 +14,37 @@ describe('lambda function', function() {
     });
   });
 });
+
+describe('lambda function', function() {
+    var index = require('index');
+    var context;
+
+    beforeEach(function() {
+	context = jasmine.createSpyObj('context', ['succeed', 'fail']);
+	index.dynamodb = jasmine.createSpyObj('dynamodb', ['scan']);
+    });
+
+    describe('popular answers', function() {
+	it('requests problems with the given problem number', function() {
+	    index.popularAnswers({problemNumber: 42}, context);
+	    expect(index.dynamodb.scan).toHaveBeenCalledWith({
+		FilterExpression: "problemId = :problemId",
+		ExpressionAttributeValues: {":problemId": 42},
+		TableName: "learnjs"
+	    }, jasmine.any(Function));
+	});
+
+	it('groups answers by minified code', function() {
+	    index.popularAnswers({"problemNumber": 1}, context);
+	    index.dynamodb.scan.calls.first().args[1](undefined,
+						      {Items: [
+							  {answer: "true"},
+							  {answer: "true"},
+							  {answer: "true"},
+							  {answer: "!false"},
+							  {answer: "!false"}
+						      ]});
+	    expect(context.succeed).toHaveBeenCalledWith({"true":3, "!false":2});
+	});
+    });
+});
